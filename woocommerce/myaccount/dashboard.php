fa70f2116eb9f3b1c0a9de2b4d945ffe2869730b
jacobwarmstrong <jacob@rootlessdesigns.com>
2021-11-16 12:10:19 -0500

    add je-dunn on customer dashboard

---
+++ woocommerce/myaccount/dashboard.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * My Account Dashboard
+ *
+ * Shows the first intro screen on the account dashboard.
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/dashboard.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see         https://docs.woocommerce.com/document/template-structure/
+ * @package     WooCommerce/Templates
+ * @version     4.4.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+$allowed_html = array(
+	'a' => array(
+		'href' => array(),
+	),
+);
+?>
+
+<p>
+	<?php
+	printf(
+		/* translators: 1: user display name 2: logout url */
+		wp_kses( __( 'Hello %1$s (not %1$s? <a href="%2$s">Log out</a>)', 'woocommerce' ), $allowed_html ),
+		'<strong>' . esc_html( $current_user->display_name ) . '</strong>',
+		esc_url( wc_logout_url() )
+	);
+	?>
+</p>
+
+<p>
+	<?php
+	/* translators: 1: Orders URL 2: Address URL 3: Account URL. */
+	$dashboard_desc = __( 'From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">billing address</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce' );
+	if ( wc_shipping_enabled() ) {
+		/* translators: 1: Orders URL 2: Addresses URL 3: Account URL. */
+		$dashboard_desc = __( 'From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">shipping and billing addresses</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce' );
+	}
+	printf(
+		wp_kses( $dashboard_desc, $allowed_html ),
+		esc_url( wc_get_endpoint_url( 'orders' ) ),
+		esc_url( wc_get_endpoint_url( 'edit-address' ) ),
+		esc_url( wc_get_endpoint_url( 'edit-account' ) )
+	);
+	?>
+</p>
+
+<?php if ( strpos( do_shortcode('[woocommerce_member_active_plans]'), 'JE Dunn') ) {
+    $product_cats = get_terms(array(
+        'taxonomy' => 'product_cat',
+        'child_of' => 247
+    ));
+    
+    echo '<div class="container"><h1>JE Dunn Products</h1><div class="row">';
+
+    foreach ( $product_cats as $term ) {
+        
+
+        // The $term is an object, so we don't need to specify the $taxonomy.
+        $term_link = get_term_link( $term );
+        
+        $term_desc = $term->description;
+
+        // If there was an error, continue to the next term.
+        if ( is_wp_error( $term_link ) ) {
+            continue;
+        }
+
+        
+        echo '<div class="col-4"><h3><a href="' . esc_url( $term_link ) . '">' . $term->name . '</a></h3><p>' . $term_desc . '</p></div>';
+    }
+    echo '</div></div>';
+}
+?>
+
+
+<?php
+	/**
+	 * My Account dashboard.
+	 *
+	 * @since 2.6.0
+	 */
+	do_action( 'woocommerce_account_dashboard' );
+
+	/**
+	 * Deprecated woocommerce_before_my_account action.
+	 *
+	 * @deprecated 2.6.0
+	 */
+	do_action( 'woocommerce_before_my_account' );
+
+	/**
+	 * Deprecated woocommerce_after_my_account action.
+	 *
+	 * @deprecated 2.6.0
+	 */
+	do_action( 'woocommerce_after_my_account' );
+
+/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
